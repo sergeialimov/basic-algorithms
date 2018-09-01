0b61c55c1aba95a88b5485559d994bb3361d02d5
Sergei Alimov <8912652@gmail.com>
2018-09-01 23:58:33 +0300

    add scenario for 0

--- factorial.js
+++ factorial.js
@@ -1,4 +1,8 @@
 function factorialize(num) {
+  if (num === 0) { 
+    return 1;
+  }
+  
   let fact = 1;
   for (let i = 1; i < num+1; i += 1) {
     fact *= i;
@@ -6,4 +10,4 @@ function factorialize(num) {
   return fact;
 }
 
-console.log(factorialize(5));
+console.log(factorialize(0));
